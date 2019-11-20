d6f4aeb7ab9284b8d2cce7584c0d9e8a60176f1b
Allen Lister <allenlister@yahoo.co.uk>
2019-11-20 16:14:43 +0100

    SQLite - connect to db

---
+++ sqlite/sqlite_connect_to_db.pl
@@ -0,0 +1,16 @@
+# will connect to a db
+# will create db if it does not exist and has enough authorization
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+
+my $driver   = "SQLite"; 
+my $database = "test.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $userid = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) 
+   or die $DBI::errstr;
+
+print "Opened database successfully\n";
